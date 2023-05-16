177caf927d5f29de451308c5e1e2e1d90489657b
HrushiAhire <129588669+HrushiAhire@users.noreply.github.com>
2023-05-17 01:38:51 +0530

    Add files via upload

---
+++ Assignment 19/Assignment19-3.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    if(iRow != iCol)
+    {
+        printf("Invalid Input\n");
+        return;
+    }
+    
+    for(i=1; i<=iRow; i++)
+    {
+        for(j=1; j<=iCol; j++)
+        {
+            if(i==j)
+            {
+                printf("$\t");
+            }
+            else if(i>j)
+            {
+                printf("#\t");
+            }
+            else
+            {
+                printf("*\t");
+            }
+        }
+        printf("\n\n");
+    }
+
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter of Rows:\n");
+    scanf("%d",&iValue1);
+
+    printf("Enter of Columns:\n");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
